42bcfb60c241ac8a114bd781636f3efd6c053936
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-03-06 16:19:34 -0300

    RAD

---
+++ main.py
@@ -0,0 +1,27 @@
+'''arquivo = open("texto.txt","r")
+print(arquivo.read())'''
+'''print(arquivo.readline())
+print(arquivo.readline())
+print(arquivo.seek(0))
+print(arquivo.readlines())
+#print(arquivo.seek(0))'''
+'''print(arquivo.tell())
+arquivo.close()
+print(arquivo.closed)'''
+'''print(arquivo.read())
+print(arquivo.seek(0))
+print(arquivo.read(8))'''
+'''print(arquivo.name)
+print(arquivo.mode)
+print(arquivo.closed)'''
+
+'''arquivo = open("novo.txt","w")
+arquivo.write("Arquivo de escrita")
+arquivo.close()
+print(arquivo.closed)'''
+
+'''arquivo = open("frutas.txt","w")
+arquivo.write("banana")
+arquivo.write("uva")
+arquivo.write("mamao")
+arquivo.close()'''
