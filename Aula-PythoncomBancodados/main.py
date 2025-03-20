32302440c5274797b70527f08c9eb4932d2a745f
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-03-20 19:08:51 -0300

    update base-python_

--- Aula-PythoncomBancodados/main.py
+++ Aula-PythoncomBancodados/main.py
@@ -1,5 +1,24 @@
 import sqlite3
 banco = sqlite3.connect('database.db')
 cursor = banco.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Pessoa(
-)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Pessoa(cpf INTEGER PRIMARY KEY,
+               nome TEXT NOT NULL,
+               nascimento DATE NOT NULL,
+               oculos BOOLEAN NOT NULL);
+''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Marca(
+               id INTEGER NOT NULL,
+               nome TEXT NOT NULL,
+               sigla CHARACTER(2) NOT NULL,
+               PRIMARY KEY(id)
+               );''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Veiculo(
+               placa CHARACTER(7) NOT NULL,
+               ano INTEGER NOT NULL,
+               cor TEXT NOT NULL,
+               proprietario INTEGER NOT NULL,
+               marca INTEGER NOT NULL,
+               PRIMARY KEY(placa),
+               FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
+               FOREIGN KEY(marca) REFERENCES Marca(id)
+               );''')
