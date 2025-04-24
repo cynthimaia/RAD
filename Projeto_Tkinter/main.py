d1bef44dffd382f5d8e96840b0c007af77e8e6b8
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-04-24 15:53:35 -0300

    opdates__

---
+++ Projeto_Tkinter/main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+import modelo
+class PrincipalBD():
+    def __init__(self, win):
+       self.objbd = modelo.AppBd()
+       self.janela = win
+
+       self.treeProdutos = ttk.Treeview(self.janela,
+                                        columns=("Código do produto",
+                                           "Nome", 
+                                           "Preco"))
+       self.treeProdutos.heading("Código do produto", text= "Id: ")
+       self.treeProdutos.heading("Nome", text="Nome: ")
+       self.treeProdutos.heading("Preco", text="Preco: ")
+       self.treeProdutos.pack()
+
+       self.nome = tk.Label(self.janela, text="Nome: ")
+       self.nome.pack()
+       self.entrynome = tk.Entry(self.janela)
+       self.entrynome.pack()
+       self.preco = tk.Label(self.janela,
+                             text="Preco")
+       self.preco.pack()
+       self.entrypreco = tk.Entry(self.janela)
+       self.entrypreco.pack()
+    
+    def cadastrarProduto(self):
+       name = self.entrynome.get()
+       price = float(self.entrypreco.get())
+       self.objbd.inserirdados(name, price)
+janela = tk.Tk() #criar a janela principal
+product_app = PrincipalBD(janela)
+#janela.title("Bem vindo ao sistema de cadastro!!")
+janela.geometry("900x700")
+janela.mainloop() 
+#apresentar janela, ate que o usuario feche
--- Projeto_Tkinter/modelo.py
+++ Projeto_Tkinter/modelo.py
@@ -1,11 +1,14 @@
 import sqlite3
 class AppBd():
+    def __init__(self):
+        self.create_table()
     def abrirconexao(self):
         try:
             self.connect = sqlite3.connect("database.db")
         except sqlite3.Error as erro:
             print("Falha ao se conectar ao banco de dados", erro)
     def create_table(self):
+        self.abrirconexao()
         create_table_query = """CREATE TABLE IF NOT EXISTS products(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 name TEXT NOT NULL,
@@ -21,12 +24,14 @@ class AppBd():
                 self.connect.close()
                 print("A conexao com o sqlite foi fechada.")
     def inserirdados(self, name, price):
+        self.abrirconexao()
         insert_query = """ INSERT INTO products
           (name, price) VALUES (?, ?)"""
         try:
             cursor = self.connect.cursor()
             cursor.execute(insert_query, (name, price))
             print("Produto cadastrado com sucesso!!")
+            self.connect.commit()
         except sqlite3.Error as erro:
             print("Falha ao inserir produto")
         finally:
@@ -35,6 +40,7 @@ class AppBd():
                 self.connect.close()
                 print("A conexao com o sqlite foi fechada!!")
     def select_all_products(self):
+        self.abrirconexao()
         select_query = """SELECT * FROM products"""
         products = []
         try:
@@ -49,8 +55,9 @@ class AppBd():
                 cursor.close()
                 self.connect.close()
                 print("A conexao com o sqlite foi fechada")
-            return self.products
+        return products
     def update_product(self, id, name, price):
+        self.abrirconexao()
         update_query = """UPDATE products SET name = ?, price = ?
         WHERE id = ?"""
         try:
@@ -66,6 +73,7 @@ class AppBd():
                 self.connect.close()
                 print("A conexão com o sqlite foi fechada.")
     def delete_product(self, id):
+        self.abrirconexao()
         delete_query = """DELETE FROM products WHERE id=?"""
         try:
             cursor = self.connect.cursor()
