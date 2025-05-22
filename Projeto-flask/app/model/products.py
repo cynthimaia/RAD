932a80c3ceab8cf7d9f85e2e5adfbf191657b2bc
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-05-22 19:44:20 -0300

    model

--- Projeto-flask/app/model/products.py
+++ Projeto-flask/app/model/products.py
@@ -5,3 +5,29 @@ class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     price = db.Column(db.Float)
+
+    def __init__(self,name, price):
+        self.name = name
+        self.price = price
+
+    def cadastrar_produto(self, name, price):
+        try:
+            add_banco = Products(name, price)
+            db.session.add(add_banco)
+            db.session.commit()
+        except Exception as erro:  
+            print("Erro ao cadastrar o produto", erro)  
+
+    def atualizar_produto(self,id, name, price ):
+        try:
+            db.session.query(Products).filter(Products.id == id).update({"name":name, "price":price})
+            db.session.commit()
+        except Exception as e:  
+            print("Erro ao atualizar o produto", e)
+    def deletar_produto(self,id):
+        try:
+            db.session.query(Products)
+            .filter(Products.id == id).delete()
+            db.session.commit()
+        except Exception as e:
+            print("Erro ao deletar produto",e)
